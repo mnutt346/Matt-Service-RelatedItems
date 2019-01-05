5e1adbb01e0ebebf323ab451ecaff73af6b57d91
gizmo <gizmo@Gremlin.local>
2019-01-04 19:35:08 -0600

    attempting to deploy part III

--- db/index.js
+++ db/index.js
@@ -1,7 +1,11 @@
 const { Pool } = require('pg')
+require('dotenv').config()
 
 const pool = new Pool({
-    database: 'projects'
+    database: 'projects',
+    host: process.env.DB_HOST || '127.0.0.1',
+    user: process.env.DB_USER || 'student',
+    password: process.env.DB_PASSWORD || 'student'
 })
 
 module.exports = {
