c816b1206fbd51056040cdee48532d5e7432455a
Michael Nutt <mnutt346@gmail.com>
2019-01-15 13:24:33 -0600

    Update pm2 config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,10 +8,10 @@ module.exports = {
   deploy: {
     production: {
       user: "ubuntu",
-      host: "ec2-3-90-224-56.compute-1.amazonaws.com",
-      key: "~/.ssh/betterJumpBackend.pem",
+      host: "ec2-54-84-25-47.compute-1.amazonaws.com",
+      key: "~/.ssh/betterJumpService.pem",
       ref: "origin/master",
-      repo: "git@github.com:mnutt346/Matt-Service-RelatedItems.git",
+      repo: "https://github.com/mnutt346/Matt-Service-RelatedItems.git",
       path: "/home/ubuntu/",
       "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
     }
